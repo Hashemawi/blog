58bf3b98963f75bd718bacda5215a55c9af41fe8
Hashem Bahbahani <hashembahbahani@Hashems-MacBook-Pro.local>
2017-11-12 20:20:13 +0300

    QuerySet ordering

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,14 @@ class Post(models.Model):
 	title = models.CharField(max_length=150)
 	content = models.TextField()
 	updated=models.DateTimeField(auto_now=True)
+	img = models.ImageField(null=True, blank=True)
+
 
 	def __str__(self):
 		return self.title
 
 	def get_detail_url(self):
 		return reverse("detail",kwargs={"post_id":self.id})
+
+	class meta:
+		ordering = ["-title", "-updated"]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-
+from .forms import PostForm
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_home(request):
 
@@ -13,6 +15,17 @@ def post_home(request):
 
 def post_list(request):
 	objects = Post.objects.all()
+	#objects = Post.objects.all().order_by('-title','-id')
+	paginator = Paginator(objects, 6)
+	number = request.GET.get('page')
+	
+	try:
+		objects = paginator.page(number)
+	except PageNotAnInteger:
+		objects = paginator.page(1)
+	except EmptyPage:
+		objects = paginator.page(paginator.num_pages)
+
 	context = {
 	"post_items": objects,
 	} 
@@ -25,3 +38,35 @@ def post_detail(request, post_id):
 	"item": item,
 	} 
 	return render(request, "detail.html", context)
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request,"Hey, you just added a blog post!")
+		return redirect("list")
+	context = {
+	'form': form
+	}
+	return render(request,"post_create.html", context)
+
+def post_update(request, post_id):
+	item = Post.objects.get(id=post_id)
+
+	form = PostForm(request.POST or None, request.FILES or None, instance=item)
+
+	if form.is_valid():
+		form.save()
+		messages.info(request,"Hey, you just changed a blog post!")
+		return redirect("list")
+	context = {
+	'form': form,
+	'item': item,
+	}
+	return render(request,"post_update.html", context)
+
+def post_delete(request, post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request, "Nooooooooooo")
+	return redirect("list")
+
